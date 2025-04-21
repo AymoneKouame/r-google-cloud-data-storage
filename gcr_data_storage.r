76fcf2f4ca014183c0e853b13090043a1a578a00
Aymone Kouame <34042185+AymoneKouame@users.noreply.github.com>
2025-04-21 14:02:44 -0500

    Update gcr_data_storage.r: added delete_saved_data + other things

--- gcr_data_storage.r
+++ gcr_data_storage.r
@@ -18,9 +18,13 @@ default_directory = ''
 README <- function()
     cat("
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ README: How to use gcr_data_storage?~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Author: Aymone Kouame
+Date Created: 03/26/2025
+Date Modified: 04/21/2025
  
 gcr_data_storage lets you easily move data between your development environment (e.g. Jupyter Notebook) and your Google Cloud Workspace bucket. 
-It integrates the command line tool gsutil.
+It integrates the command line tool gcloud storage.
 
   * Use `save_data_to_bucket(data, filename)` to save data from your development environment to the bucket.
 
@@ -29,6 +33,8 @@ It integrates the command line tool gsutil.
   * Use `copy_from_bucket_to_bucket(origin_filename, destination_bucket)` to copy data between different directories within the same bucket or between two different buckets owned by the user.
 
   * Use `list_saved_data()` to obtain a list of data saved in the bucket or the disk.
+
+  * Use `delete_saved_data(filename)` to delete data saved in the bucket or the disk. The default = 'bucket'. For the disk, `bucket_or_disk = 'disk'`. 
 
 gcr_data_storage was originally written to be used within the All of Us Researcher Workbench environment but can be used in other Google Cloud Environments.
 
@@ -61,9 +67,10 @@ save_data_to_bucket <- function(data
     print(str_glue("
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Saving data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 To location =  '{bucket}/{directory}'
-    "))
+    \n\n"))
 
-    full_filename <- str_glue('{bucket}/{directory}/{filename}') 
+    full_filename <- str_glue('{bucket}/{directory}/{filename}')
+    full_filename = gsub('gs:/','gs://', gsub('//','/', full_filename))
 
     if (any(class(data) %in% c('tbl_df','tbl','data.frame')) == TRUE){
         write.table(data, filename)
@@ -78,7 +85,9 @@ Your file extension is not a dataframe or plot. We will assume your data is alre
         
         }      
     
-    res <- system(str_glue("gsutil cp {filename} {full_filename}"), intern= F)
+    print(str_glue(str_glue("[Running command: `system('gcloud storage cp {filename} {full_filename}', intern= F)`]\n")))
+    res <- system(str_glue("gcloud storage cp {filename} {full_filename}"), intern= F)
+    
     print(str_glue("Data saved as '{filename}' in location."))
     res_rm <- system(str_glue("rm {filename}"))
   }
@@ -95,10 +104,11 @@ read_data_from_bucket<- function(filename
     print(str_glue("
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Reading data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 From location =  '{bucket}/{directory}'
-    "))
+    \n\n"))
     
-    full_filename <- str_glue('{bucket}/{directory}/{filename}') 
-    res <- system(str_glue("gsutil cp {full_filename} {filename}"), intern=F)
+    full_filename <- str_glue('{bucket}/{directory}/{filename}')
+    print(str_glue(str_glue("[Running command: `system('gcloud storage cp {full_filename} {filename}', intern= F)`]\n")))
+    res <- system(str_glue("gcloud storage cp {full_filename} {filename}"), intern=F)
     
     if (grepl(pattern = '.png|.jpeg|.bmp|.tiff|.pdf|.emf', filename)){
         data <- load.image(file = filename)
@@ -140,9 +150,10 @@ copy_from_bucket_to_bucket<- function(origin_filename
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ copying data between buckets ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     From {origin_fullfilename}
     To {dest_fullfilename}
-        "))
+        \n\n"))
     
-     res <- system(str_glue("gsutil cp {origin_fullfilename} {dest_fullfilename}"), intern= F)
+     print(str_glue(str_glue("[Running command: `system('gcloud storage cp {origin_fullfilename} {dest_fullfilename}', intern= F)`]\n")))
+     res <- system(str_glue("gcloud storage cp {origin_fullfilename} {dest_fullfilename}"), intern= F)
     
      if (res ==1){print('Data successfully copied.')}
 
@@ -162,15 +173,61 @@ list_saved_data<- function(bucket_or_disk = 'bucket'
         if (is.null(bucket)== TRUE){bucket = default_bucket}
         location = gsub('//', '/', str_glue("{bucket}/{directory}/{pattern}"))
         location = gsub('gs:/', 'gs://', location)
-        print(str_glue('In {location}\n'))
-        system(str_glue("gsutil ls {location}"), intern=T) 
+        print(str_glue('In {location}\n\n'))
+        
+        print(str_glue(str_glue("[Running command: `system('gcloud storage ls {location}', intern= F)`]\n")))
+        system(str_glue("gcloud storage ls {location}"), intern=T) 
                 
    } else if ((tolower(bucket_or_disk) %in% c('persistent disk','persistent_disk', 'disk')) 
              | (!tolower(bucket_or_disk) %in% c('bucket',''))){
         if (is.null(directory)== TRUE){directory = ''}
         location = gsub('/*', '', str_glue("{directory}/{pattern}"))
         print(str_glue('In disk {location}\n\n'))
+        
+        print(str_glue(str_glue("[Running command: `system('ls {directory}{pattern}', intern= F)`]\n")))        
         system(str_glue("ls {directory}{pattern}"), intern=T)
+    
+     }
+    }
+
+
+delete_saved_data<- function(filename
+                          , bucket_or_disk = 'bucket'
+                          , bucket = NULL, directory = NULL){
+    
+    print("
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Deleting data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Please USE WITH CAUTION.
+    \n\n")
+    
+    if (is.null(directory) == TRUE){directory = default_directory}
+    if (is.null(bucket)== TRUE){bucket = default_bucket}
+    
+    if (tolower(bucket_or_disk) %in% c('bucket','')){
+        location = gsub('//', '/', str_glue("{bucket}/{directory}/{filename}"))
+        location = gsub('gs:/', 'gs://', location)
+        print(str_glue('Deleting {location}\n'))
+        
+        delete_answer  = readline(str_glue("PLEASE CONFIRM DELETION OF '{location}'. TYPE 'DELETE' then press ENTER."))
+        if (delete_answer == 'DELETE'){
+            print(str_glue("[Running command: `system('gcloud storage rm {location}', intern=T)'`]\n"))
+            system(str_glue("gcloud storage rm {location}"), intern=T)
+            print("\nFile deleted")}
+        
+        else {print("\nDeletion CANCELLED.")}
+                
+   } else if ((tolower(bucket_or_disk) %in% c('persistent disk','persistent_disk', 'disk')) 
+             | (!tolower(bucket_or_disk) %in% c('bucket',''))){
+        location = str_glue("{directory}/{filename}")
+        print(str_glue('Deleting {location}\n\n'))
+        
+        delete_answer  = readline(str_glue("PLEASE CONFIRM DELETION OF '{location}'. TYPE 'DELETE' then press ENTER."))
+        if (delete_answer == 'DELETE'){
+            print(str_glue("[Running command: `system('rm {location}', intern=T)`]\n"))
+            system(str_glue("rm {location}"), intern=T)
+            print("\nFile deleted")
+            }
+        else {print("\nDeletion CANCELLED.")}
     
      }
     }
